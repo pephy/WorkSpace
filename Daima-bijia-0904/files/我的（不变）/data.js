a62b915552df94ddc36b6c365c8b78da62f817c0
Pephy Luo <luop_16@qq.com>
2019-09-18 14:44:46 +0800

    ！

---
+++ Daima-bijia-0904/files/我的（不变）/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="我的（不变）.html",d="generationDate",e=new Date(1536064924881.34),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="d09aa76b4ca84faea17092a4d089875d",q="type",r="Axure:Page",s="name",t="我的（不变）",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="c9e6c0aa20c040b9bc66411ada5d841b",Y="label",Z="friendlyType",ba="Image",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=198,bj="height",bk=353,bl="imageOverrides",bm="images",bn="normal~",bo="images/首页/u342.jpg",bp="masters",bq="objectPaths",br="c9e6c0aa20c040b9bc66411ada5d841b",bs="scriptId",bt="u1291";
+return _creator();
+})());
